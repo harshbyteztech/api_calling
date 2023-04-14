00156efa4e9c549a184bb693183d4fc1fe675aa5
PoriyaBytez <poriyabytez@gmail.com>
2023-04-14 18:11:04 +0530

    UPDATE

--- lib/Screen/faceBook_authentication.dart
+++ lib/Screen/faceBook_authentication.dart
@@ -13,14 +13,11 @@ class _LoginscreenState extends State<Loginscreen> {
   TextEditingController _controller = TextEditingController();
 
   Future<UserCredential> signInWithFacebook() async {
-    // Trigger the sign-in flow
     final LoginResult loginResult = await FacebookAuth.instance.login();
 
-    // Create a credential from the access token
     final OAuthCredential facebookAuthCredential =
         FacebookAuthProvider.credential(loginResult.accessToken!.token);
 
-    // Once signed in, return the UserCredential
     return FirebaseAuth.instance.signInWithCredential(facebookAuthCredential);
   }
 
